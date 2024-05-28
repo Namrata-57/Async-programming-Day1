0efd4ebacc48638b7139493ea4209116bcf680e2
Namrata-57 <namrata57desai@gmail.com>
2024-05-28 09:56:48 +0530

    changes updated

--- script.js
+++ script.js
@@ -1,49 +1,53 @@
 const h1 = document.createElement('h1');
 console.log(h1);
-
+document.body.classList.add('d-flex', 'flex-column', 'justify-content-center', 'align-items-center', 'vh-100');
 h1.classList.add('display-1', 'text-center');
 
-setTimeout( () => {
+setTimeout(() => {
     h1.innerText = 10;
-    setTimeout( () => {
+    setTimeout(() => {
         h1.innerText = 9;
-        setTimeout( () => {
+        setTimeout(() => {
             h1.innerText = 8;
-            setTimeout( () => {
+            setTimeout(() => {
                 h1.innerText = 7;
-                setTimeout( () => {
+                setTimeout(() => {
                     h1.innerText = 6;
-                    setTimeout( () => {
+                    setTimeout(() => {
                         h1.innerText = 5;
-                        setTimeout( () => {
+                        setTimeout(() => {
                             h1.innerText = 4;
-                            setTimeout( () => {
+                            setTimeout(() => {
                                 h1.innerText = 3;
-                                setTimeout( () => {
+                                setTimeout(() => {
                                     h1.innerText = 2;
-                                    setTimeout( () => {
+                                    setTimeout(() => {
                                         h1.innerText = 1;
                                         setTimeout(() => {
-                                            h1.innerText = "";
-                                            // Displaying "Happy Independence" after the countdown
+                                            h1.innerText = '';
                                             const message = document.createElement('div');
-                                            message.innerText = "Happy Independence Day!";
-                                            message.classList.add('display-1', 'text-center');
-                                            document.body.append(message);
-                                        }, 1000);
-                                    }, 1000);
-                                }, 1000);
-                            }, 1000);
-                        }, 1000);
-                    }, 1000);
-                }, 1000);
-            }, 1000);
-        }, 1000);
-    }, 1000);
-}, 1000);
+                                            message.innerText = "Happy Independence Day";
+                                            message.classList.add('display-1', 'text-center')
+                                            const image = document.createElement('img');  //we can add image
+                                            image.setAttribute('src', '/india-flag.webp');
+                                            image.setAttribute('alt', 'india-flag');
+                                            image.classList.add('rounded', 'mx-auto', 'pt-auto', 'd-block')
+                                            document.body.append(message, image);
+                                        }, 1000)
+                                    }, 1000)
+                                }, 1000)
+                            }, 1000)
+                        }, 1000)
+                    }, 1000)
+                }, 1000)
+            }, 1000)
+        }, 1000)
+    }, 1000)
+}, 1000)
+
+
 
 document.addEventListener('DOMContentLoaded', () => {
     h1.innerHTML = 10;
     document.body.append(h1);
 })
-
